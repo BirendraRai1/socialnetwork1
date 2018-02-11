d8f0d95cf72ca74c7f5d1b5a12f4169fe07fdc4f
BirendraRai1 <birendrabit123@gmail.com>
2018-02-11 19:50:02 +0530

    second commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const container = require('./container');
 container.resolve(function(users, _, admin, home, group, members, privatechat){
     
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/yallareytr', {useMongoClient: true});
+    mongoose.connect('mongodb://localhost/nehayalla', {useMongoClient: true});
     
     const app = SetupExpress();
     
